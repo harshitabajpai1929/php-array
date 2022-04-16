d703107f30aa55d12f8c1f0529329f79e29992bd
cedcoss <>
2022-04-16 17:08:56 +0530

    advance php commit

--- src/advance2.php
+++ src/advance2.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Array advance php </title>
+    <title>sub task 4 and 5</title>
     <style>
-    table,th,td{
+      table,th,td{
+        border: collapse;
         border: 2px solid black;
         width: 50%;
     }
     </style>
 </head>
 <body>
-  <h1>Sub Task 1</h1>
+  <h1>deletion and updation subtask 4 & 5 </h1>
 <table>
 <tr>
   <th>Category</th>
@@ -100,76 +101,20 @@ $products = array(
    foreach ($products as $key => $value) {
        foreach ($value as $key1 => $val1) {
             foreach($val1 as $key2 => $val2){             
-                {   
+               if ($val2["id"] == "PR003"){
+                  unset($val2);
+                  // unset($key);
+                  unset($key1);
+                  }
+              else if ($val2["id"] == "PR002"){
+                    $val2["name"] = "BIG-555";
+                  }
+
                echo "<tr><td>$key</td><td>$key1</td><td>$val2[id]</td><td>$val2[name]</td><td>$val2[brand]</td></tr>";
                 }
             } 
           } 
-        }
            echo '</table>';
          ?>
-        <br>
-        <br>
-           <h1>Sub Task 2</h1>
-           <br>
-        <table>
-        <tr>
-           <th>Category</th>
-           <th>Id</th>
-           <th>Name</th>
-           <th>Brand</th>
-        </tr>
-        <?php
-        $newarr = array("Mobile" => array(
-            array(
-            "id" => "PR004",
-            "name" => "GT-1980",
-            "brand" => "Samsung"
-              ),
-           array(
-          "id" => "PR005",
-          "name" => "IG-5467",
-          "brand" => "Motorola"
-          ),
-            array(
-          "id" => "PR006",
-          "name" => "IP-8930",
-          "brand" => "Apple"
-            )
-           )
-            );
-          foreach ($newarr as $key => $value) {
-              foreach ($value as $key1 => $val1) {
-                echo "<tr><td>$key</td><td>$val1[id]</td><td>$val1[name]</td><td>$val1[brand]</td></tr>";   
-              }   
-          }           
-             echo '</table>';
-         ?>
-         <br>
-         <h1>Sub Task 3</h1>
-        <?php
-        echo '<table>';
-         $samsung_array= array (array(
-          "id" => "PR001",
-          "name" => "MAX-001",
-          "SubCategory" => "Television",
-          "Category" => "Electronics"
-         ),
-         array(
-          "id" => "PR004",
-          "name" => "GT-1980",
-          "SubCategory" => "Mobile",
-          "Category" => "Electronics"
-            )
-         );
-          foreach ($samsung_array as $key => $value){
-               echo "<tr><td>Product ID:</td><td>$value[id]</td></tr><br>
-               <tr><td>Product Name:</td><td>$value[name]</td></tr><br>
-               <tr><td>Subcategory:</td><td>$value[SubCategory]</td></tr><br>
-               <tr><td>Category:</td><td>$value[Category]</td></tr><br>";
-           };
-          echo "</table>"
-        ?>
-       
 </body>
 </html>
